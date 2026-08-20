55632b2158eaed13767aff66aa3f261508aaf909
jshrghosh <jshrghosh80@gmail.com>
2026-08-20 11:27:14 +0530

    User Created Event in Notification Service MS

--- UserCreatedEvent.java
+++ UserCreatedEvent.java
@@ -1,16 +1,36 @@
 package com.nt.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class UserCreatedEvent {
 	private Integer id;
 	private String firstName;
-	private String lastName;
 	private String email;
-
+	
+	public void setId(Integer id) {
+		this.id=id;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName=firstName;
+	}
+	
+	public void setEmail(String email) {
+		this.email=email;
+	}
+	
+	public String getEmail() {
+		// TODO Auto-generated method stub
+		return email;
+	}
+	
+	public String getFirstName() {
+		// TODO Auto-generated method stub
+		return firstName;
+	}
+	
+	@Override
+	public String toString() {
+		return "UserCreatedEvent[id="+id+ ", firstName="+ firstName+ ", email="+email+"]";
+	}
 }
